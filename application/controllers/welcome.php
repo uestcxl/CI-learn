5e01e8e4dd2c704875fc4aa5b7c0bb9cc0c1e593
xl <865492652@qq.com>
2014-11-09 20:51:40 +0800

    bugs

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -7,10 +7,10 @@ class Welcome extends CI_Controller {
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -19,8 +19,33 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+        $this->load->helper('form');
+        $data['title'] = "Welcome to our Site";
+        $data['headline'] = "Welcome!";
+        $data['include'] = 'home';
+        $this->load->vars($data);
 		$this->load->view('welcome_message');
 	}
+
+    function contactus(){
+        $this->load->helper('url');
+         if ($this->input->post('email')){
+                    $this->load->model('MContacts','',TRUE);
+                        $this->MContacts->addContact();
+                        redirect('welcome/thankyou','refresh');
+                }else{
+                    redirect('welcome/index','refresh');
+            }
+    }
+
+
+    function thankyou(){
+          $data['title'] = "Thank You!";
+            $data['headline'] = "Thanks!";
+            $data['include'] = 'thanks';
+              $this->load->vars($data);
+              $this->load->view('welcome_message');
+    }
 }
 
 /* End of file welcome.php */
